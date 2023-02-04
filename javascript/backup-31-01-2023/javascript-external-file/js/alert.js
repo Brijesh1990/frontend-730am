874143b0ac6d1ffc313593578075dd0e6c225ab2
Brijesh1990 <bkpandey.pandey@gmail.com>
2023-02-04 09:00:56 +0530

    Add files via upload

---
+++ javascript/backup-31-01-2023/javascript-external-file/js/alert.js
@@ -0,0 +1,7 @@
+// alert('Hello guys i am just load amazon click on OK')
+// window.location='https://www.amazon.com';
+// var a='Hello world i am javascript';
+// document.write(a);
+
+alert('Hello guys i am just load amazon click on OK')
+window.location='https://www.amazon.com';
